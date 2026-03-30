100437bc29389dbfd9e9f2fa0d8b26cd4b2f8a8d
SMFDrummer <1323152000@qq.com>
2026-03-31 07:01:39 +0800

    0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
-    kotlin("plugin.serialization") version "2.2.0"
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
-    id("com.vanniktech.maven.publish") version "0.34.0"
+    kotlin("jvm") version "2.3.20"
+    kotlin("plugin.serialization") version "2.3.20"
+    id("com.google.devtools.ksp") version "2.3.6"
+    id("com.vanniktech.maven.publish") version "0.36.0"
+    id("com.gradleup.shadow") version "9.4.1"
 }
 
 group = project.group as String
@@ -12,13 +13,13 @@ dependencies {
     testImplementation(kotlin("test"))
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     // multipart/form-data serialization
     implementation("io.github.edmondantes:simple-kotlin-multipart:1.0.0")
     implementation("io.github.edmondantes:simple-kotlinx-serialization-multipart:0.1.0")
 
-    implementation(platform("io.ktor:ktor-bom:3.2.3"))
+    implementation(platform("io.ktor:ktor-bom:3.4.2"))
     implementation("io.ktor:ktor-client-core")
     implementation("io.ktor:ktor-client-cio")
     implementation("io.ktor:ktor-client-logging")
@@ -26,7 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("org.slf4j:slf4j-nop:2.1.0-alpha1")
 
-    implementation(platform("io.arrow-kt:arrow-stack:2.1.2"))
+    implementation(platform("io.arrow-kt:arrow-stack:2.2.2.1"))
     implementation("io.arrow-kt:arrow-core")
     implementation("io.arrow-kt:arrow-core-serialization")
     implementation("io.arrow-kt:arrow-fx-coroutines")
@@ -39,7 +40,7 @@ dependencies {
     implementation("io.arrow-kt:arrow-cache4k")
     implementation("io.github.nomisrev:kotlinx-serialization-jsonpath:2.0.0-alpha.1")
 
-    implementation("org.bouncycastle:bcprov-jdk18on:1.81")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.83")
 }
 
 tasks.test {
@@ -84,4 +85,15 @@ mavenPublishing {
 
     publishToMavenCentral(true)
     signAllPublications()
+}
+
+tasks.shadowJar {
+    archiveBaseName = "medal-core"
+
+    manifest {
+        attributes(
+            "Implementation-Title" to "Medal-core",
+            "Implementation-Version" to project.version
+        )
+    }
 }
